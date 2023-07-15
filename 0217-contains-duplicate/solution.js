50c99d77d262e286bb1670ee9fd4dbb426cf3e7d
Jimmy Ma <47760688+majimmy88@users.noreply.github.com>
2023-07-15 13:34:11 -0700

    Create solution.js
    Initial Solution

---
+++ 0217-contains-duplicate/solution.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function(nums) {
+    const set = new Set()
+    for(const num of nums) {
+        if(!set.has(num)) {
+            set.add(num)
+        } else {
+            return true
+        }
+    }  
+    return false
+};
