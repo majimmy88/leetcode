11d3e34218100c5e8757bbf564ae78f2b3046d31
Jimmy Ma <47760688+majimmy88@users.noreply.github.com>
2023-08-16 18:11:42 -0700

    Update solution.ts

--- 1470-shuffle-the-array/solution.ts
+++ 1470-shuffle-the-array/solution.ts
@@ -1,11 +1,10 @@
 function shuffle(nums: number[], n: number): number[] {
-    let result: number[] = []
-    let first: number[] = nums.slice(0, n)
-    let second: number[] = nums.slice(n)
+    const result: number[] = []
+    const first: number[] = nums.slice(0, n)
+    const second: number[] = nums.slice(n)
     for(let i = 0; i < n; i++) {
         result.push(first[i])
         result.push(second[i])
     }
     return result
-
 };
