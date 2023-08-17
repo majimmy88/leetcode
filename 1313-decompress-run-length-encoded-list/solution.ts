b11880e8aa92c348f099fd4533c54c6a3f4e540b
Jimmy Ma <47760688+majimmy88@users.noreply.github.com>
2023-08-16 18:08:55 -0700

    Update solution.ts

--- 1313-decompress-run-length-encoded-list/solution.ts
+++ 1313-decompress-run-length-encoded-list/solution.ts
@@ -1,13 +1,9 @@
 function decompressRLElist(nums: number[]): number[] {
     const result: number[] = []
-
     for(let i = 0; i < nums.length; i+=2) {
         let freq = nums[i]
         let val = nums[i+1]
-        while(freq > 0) {
-            result.push(val)
-            freq--
-        }
+        result.push(...Array(freq).fill(val))
     }
     return result
 };
