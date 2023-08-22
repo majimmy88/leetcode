e5144f487040eff03f7655eee5a8e350937c78e7
Jimmy Ma <47760688+majimmy88@users.noreply.github.com>
2023-08-21 18:05:04 -0700

    Update solution.ts

--- 2788-split-strings-by-separator/solution.ts
+++ 2788-split-strings-by-separator/solution.ts
@@ -2,14 +2,13 @@ function splitWordsBySeparator(words: string[], separator: string): string[] {
     const result: string[] = []
     for(const word of words) {
         let currWord = ""
-        for(let i = 0; i < word.length; i++) {
-            const currChar = word[i]
-            if(currChar === separator && currWord.length > 0) {
+        for(const char of word) {
+            if(char === separator && currWord.length > 0) {
                 result.push(currWord)
                 currWord = ""
             }
-            if(currChar !== separator) {
-                currWord += currChar
+            if(char !== separator) {
+                currWord += char
             }
         }
         if(currWord.length > 0) {
